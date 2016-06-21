5db7fd193f69238183d6007bec576d011126ad56
matheus <freitag.matheus@gmail.com>
2016-06-21 11:55:04 -0500

    projects working and garbage code deleted

--- connect.php
+++ connect.php
@@ -4,9 +4,6 @@ $conn = new PDO('mysql:host=localhost;dbname=projects;charset=utf8mb4', 'root', 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-/*
-$connect = mysql_connect($servername, $username, $password);
-$dbs = mysql_select_db($DB_name, $connect);
-*/
+
 
  ?>
--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-var listOfProjects = []
-
 var app = angular.module('portal', []);
 app.controller("projectsListCtrl", function($scope, $http){
   $http.get("projects.php").success(function(data){
     $scope.listOfProjects = data;
-      console.log(data);
   })
 });
