64ccc826ddecd74076a3e20d08e8c44e5b20da19
matheus <freitag.matheus@gmail.com>
2016-06-21 11:53:04 -0500

    projects working

--- connect.php
+++ connect.php
@@ -1,11 +1,12 @@
 <?php
-//Connection with the Database
-$servername         = "127.0.0.1";
-$username           = "root";
-$password           = "";
-$DB_name            = "Projects";
 
+$conn = new PDO('mysql:host=localhost;dbname=projects;charset=utf8mb4', 'root', '');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+/*
 $connect = mysql_connect($servername, $username, $password);
 $dbs = mysql_select_db($DB_name, $connect);
+*/
 
  ?>
--- script.js
+++ script.js
@@ -1,54 +1,9 @@
-var dictionaryOfProjects = [];
+var listOfProjects = []
 
-//When document is ready, execute PHP <-> MySQL script:
-$(document).ready(function(){
-  //Retrieve the data from the database (works with the config.php script)
-  $.ajax({
-    //Basic configuration required
-    url: 'config.php',
-    data: "", //Not sending anything to the database, so remains null
-    dataType: 'json',
-    success: function(data){ //In case of success
-      for (var x in data){ //For every row in the data
-        var row = data[x]; //Separate the row
-        dictionaryOfProjects.push({
-          professor: row[1],
-          titulo: row[2]
-        });
-      }
-    }
-  });
-
-  //Control the toggle when Filter Search Button is clicked
-  var down = true;
-  $( "#filter" ).click(function() {
-    $("#refine-search").slideToggle("slow");
-    $("#refine-search").css("display", "block");
-    if (down){
-      $( ".demo-card-wide.mdl-card" ).animate({ "bottom": "-=120px" }, "slow" );
-      $( "#footer" ).animate({ "bottom": "-=120px" }, "slow" );
-      down=false;
-    }
-    else{
-      $( ".demo-card-wide.mdl-card" ).animate({ "bottom": "+=120px" }, "slow" );
-      down = true;
-    }
-  });
-});
-
-//AngularJS scripts goes here:
-var app = angular.module("portal", []);
-app.controller("projectsListCtrl", function($scope){
-  $scope.listOfProjects = dictionaryOfProjects;
-});
-
-
-var databaseApp = angular.module("databaseApp", []);
-databaseApp.controller("databaseCtrl",function($scope, $http){
-  $scope.insertDatabase=function(){
-    $http.post("register.php", {'databaseTitle':$scope.databaseTitle,'databaseDescription':$scope.databaseDescription})
-    .success(function(data,status,headers,config){
-      console.log("data successufully inserted!");
-    });
-  }
+var app = angular.module('portal', []);
+app.controller("projectsListCtrl", function($scope, $http){
+  $http.get("projects.php").success(function(data){
+    $scope.listOfProjects = data;
+      console.log(data);
+  })
 });
