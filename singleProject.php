7016b75aea2ae1e10958214b5d098500643d04f7
matheus <freitag.matheus@gmail.com>
2016-09-14 10:47:48 -0300

    minor bugs fixed

--- singleProject.php
+++ singleProject.php
@@ -16,34 +16,6 @@ $json = json_encode( $result );
 echo $json;
 
 
-/*
-// a query get all the records from the users table
-$sql = 'SELECT id, name, email FROM users';
-
-// use prepared statements, even if not strictly required is good practice
-$stmt = $dbh->prepare( $sql );
-
-// execute the query
-$stmt->execute();
-
-// fetch the results into an array
-$result = $stmt->fetchAll( PDO::FETCH_ASSOC );
-
-// convert to json
-$json = json_encode( $result );
-
-// echo the json string
-echo $json;
-
-
-$request = json_decode( file_get_contents('php://input') );
-$variable = $request->id;
-
-$stmt = $conn->query('SELECT * FROM projects WHERE id="'.$variable.'"');
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-$data =(json_encode($results));
-*/
 
 
 
